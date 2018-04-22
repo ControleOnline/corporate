108a6461d87e1c2b74326ec5007ce6b1a492c0db
luizk <luizk@192.168.0.5>
2018-04-22 19:27:59 -0300

    Change modules to vendor

--- src/Corporate/Controller/DefaultController.php
+++ src/Corporate/Controller/DefaultController.php
@@ -18,6 +18,7 @@ class DefaultController extends \Core\Controller\CompanyController {
         if (ErrorModel::getErrors()) {
             return $this->_view;
         }
+        $this->_view->setVariable('forceNotLoggedInLayout', true);
         if (!$this->_userModel->loggedIn()) {
             return \Core\Helper\View::redirectToLogin($this->_renderer, $this->getResponse(), $this->getRequest(), $this->redirect());
         } else {
@@ -30,12 +31,59 @@ class DefaultController extends \Core\Controller\CompanyController {
                         'documento' => $cnpj
             ));
 
-            echo '<pre>';
-            print_r($data);
-            echo '</pre>';
+            if ($data && $data['CPF']) {
+                $document = $this->_userModel->getLoggedUserPeople()->getDocument()[0]->getDocument();
+                foreach ($data['CPF'] AS $cpf) {
+                    if ($document == $cpf) {
+                        /*
+                         * É socio
+                         */
+                        $is_business_partner = true;
+                    }
+                }
 
+                if ($is_business_partner) {
+                    if (count($data['CPF']) > 1) {
+                        /*
+                         * Existem outros socios
+                         */
+                        return $this->redirectToLogin($this->_renderer, $this->getResponse(), $this->redirect(), '/corporate/conference-business-partner');
+                    } else {
+                        /*
+                         * Verificar endereço
+                         */
+                        return $this->redirectToLogin($this->_renderer, $this->getResponse(), $this->redirect(), '/corporate/conference-address');
+                    }
+                } else {
+                    /*
+                     * Procuração
+                     */
+                    return $this->redirectToLogin($this->_renderer, $this->getResponse(), $this->redirect(), '/corporate/create-procuration');
+                }
+            }
             exit;
         }
+    }
+
+    public function createProcurationAction() {
+        $this->_view->setVariable('forceNotLoggedInLayout', true);
+        return $this->_view;
+    }
+
+    public function conferenceBusinessPartnerAction() {
+        $this->_view->setVariable('forceNotLoggedInLayout', true);
+        return $this->_view;
+    }
+
+    public function conferenceAddressAction() {
+        $this->_view->setVariable('forceNotLoggedInLayout', true);
+        return $this->_view;
+    }
+
+    public function redirectTo($renderer, $response, $redirect, $url) {
+        $redirect->toUrl($renderer->basePath($url));
+        $response->sendHeaders();
+        exit;
     }
 
 }
